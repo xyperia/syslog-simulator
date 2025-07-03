aec4f042d57678ec2bbb0bdd2070b6603b7bb841
Septian Parapak <septianparapak@gmail.com>
2025-07-03 21:05:10 +0700

    Initial Commit

---
+++ SimpleLogForwarder/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SimpleLogForwarder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sendOnce = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.cmbProtocol = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numInterval = new System.Windows.Forms.NumericUpDown();
+            this.btnSendAlways = new System.Windows.Forms.Button();
+            this.SchedulerTimer = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbLogType = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.eventMsg = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sendOnce
+            // 
+            this.sendOnce.Location = new System.Drawing.Point(315, 379);
+            this.sendOnce.Name = "sendOnce";
+            this.sendOnce.Size = new System.Drawing.Size(138, 36);
+            this.sendOnce.TabIndex = 0;
+            this.sendOnce.Text = "Send Once";
+            this.sendOnce.UseVisualStyleBackColor = true;
+            this.sendOnce.Click += new System.EventHandler(this.sendOnce_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(156, 33);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(168, 26);
+            this.txtIP.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(382, 33);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(99, 26);
+            this.txtPort.TabIndex = 2;
+            // 
+            // cmbProtocol
+            // 
+            this.cmbProtocol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProtocol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbProtocol.FormattingEnabled = true;
+            this.cmbProtocol.Location = new System.Drawing.Point(568, 31);
+            this.cmbProtocol.Name = "cmbProtocol";
+            this.cmbProtocol.Size = new System.Drawing.Size(121, 28);
+            this.cmbProtocol.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Destination IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(487, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Protocol :";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(37, 133);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(652, 231);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.Text = "Put your log here. Each line will be treated as a different log. If you add more " +
+    "than one line, then it will be randomize or send in order. Select \"Custom\" as th" +
+    "e Log Type to modify this.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message :";
+            // 
+            // numInterval
+            // 
+            this.numInterval.Location = new System.Drawing.Point(603, 382);
+            this.numInterval.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.numInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numInterval.Name = "numInterval";
+            this.numInterval.Size = new System.Drawing.Size(48, 26);
+            this.numInterval.TabIndex = 10;
+            this.numInterval.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btnSendAlways
+            // 
+            this.btnSendAlways.Location = new System.Drawing.Point(459, 379);
+            this.btnSendAlways.Name = "btnSendAlways";
+            this.btnSendAlways.Size = new System.Drawing.Size(138, 36);
+            this.btnSendAlways.TabIndex = 11;
+            this.btnSendAlways.Text = "Send every";
+            this.btnSendAlways.UseVisualStyleBackColor = true;
+            this.btnSendAlways.Click += new System.EventHandler(this.btnSendAlways_Click);
+            // 
+            // SchedulerTimer
+            // 
+            this.SchedulerTimer.Tick += new System.EventHandler(this.SchedulerTimer_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(657, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "sec";
+            // 
+            // cmbLogType
+            // 
+            this.cmbLogType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLogType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbLogType.FormattingEnabled = true;
+            this.cmbLogType.Location = new System.Drawing.Point(491, 78);
+            this.cmbLogType.Name = "cmbLogType";
+            this.cmbLogType.Size = new System.Drawing.Size(198, 28);
+            this.cmbLogType.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(402, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Log Type :";
+            // 
+            // eventMsg
+            // 
+            this.eventMsg.Enabled = false;
+            this.eventMsg.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.eventMsg.Location = new System.Drawing.Point(37, 436);
+            this.eventMsg.Name = "eventMsg";
+            this.eventMsg.Size = new System.Drawing.Size(652, 80);
+            this.eventMsg.TabIndex = 16;
+            this.eventMsg.Text = "Brought to you by XYPERIA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 410);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Log :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 563);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.eventMsg);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbLogType);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSendAlways);
+            this.Controls.Add(this.numInterval);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbProtocol);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.sendOnce);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Syslog Simulator v1.1";
+            ((System.ComponentModel.ISupportInitialize)(this.numInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendOnce;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.ComboBox cmbProtocol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numInterval;
+        private System.Windows.Forms.Button btnSendAlways;
+        private System.Windows.Forms.Timer SchedulerTimer;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbLogType;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox eventMsg;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SimpleLogForwarder/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SimpleLogForwarder
+{
+    public partial class Form1 : Form
+    {
+        string appTitle = "Syslog Simulator v1.1";
+
+        private List<string> messageLines = new List<string>();
+        private int currentLineIndex = 0;
+        private Timer schedulerTimer;
+
+        string[] ipCat1 = { "10.100.33.97", "10.100.33.137", "10.100.33.181", "10.100.33.208", "10.100.33.189" };
+        string[] ipCat2 = { "10.100.34.134", "10.100.34.192", "10.100.34.111", "10.100.34.135", "10.100.34.130" };
+        string[] ipCat3 = { "10.99.34.28", "10.99.34.137", "10.98.37.211", "10.99.34.138", "10.99.37.127" };
+        string[] ports = { "12020", "10999", "8088", "9090", "8000" };
+
+        
+
+        Dictionary<string, string> logSample = new Dictionary<string, string>()
+        {
+            { "Fortigate", "date=2019-03-31 time=06:42:54 logid=\"0002000012\" type=\"traffic\" subtype=\"multicast\" level=\"notice\" vd=\"vdom1\" eventtime=1554039772 srcip=172.16.200.55 srcport=60660 srcintf=\"port25\" srcintfrole=\"undefined\" dstip=230.1.1.2 dstport=7878 dstintf=\"port3\" dstintfrole=\"undefined\" sessionid=1162 proto=17 action=\"accept\" policyid=1 policytype=\"multicast-policy\" service=\"udp/7878\" dstcountry=\"Reserved\" srccountry=\"Reserved\" trandisp=\"noop\" duration=22 sentbyte=5940 rcvdbyte=0 sentpkt=11 rcvdpkt=0 appcat=\"unscanned\"" },
+            { "Mikrotik", "Feb 20 04:38:02 192.168.202.101 id=firewall sn=C0EAE45CA55 time=\"2021-02-20 12:54:43\" fw=188.126.145.3 pri=6 c=1 m=911 msg=\"Added host entry to dynamic address object\" n=6776584 note=\"FQDN=*.microsoft.com; TTL=56; Host=20.49.150.241\" fw_action=\"NA\"" }
+        };
+
+    public Form1()
+        {
+            InitializeComponent();
+
+            cmbProtocol.Items.Add("UDP");
+            cmbProtocol.Items.Add("TCP");
+            cmbProtocol.SelectedIndex = 0;
+
+            cmbLogType.Items.Add("Fortigate");
+            cmbLogType.Items.Add("Mikrotik");
+            cmbLogType.Items.Add("Custom");
+
+
+            cmbLogType.SelectedIndex = 0;
+            cmbLogType.SelectedIndexChanged += cmbLogType_SelectedIndexChanged;
+
+            schedulerTimer = new Timer();
+            schedulerTimer.Tick += SchedulerTimer_Tick;
+
+            // Load saved settings
+            txtIP.Text = Properties.Settings.Default.LastIP;
+            txtPort.Text = Properties.Settings.Default.LastPort;
+            cmbProtocol.SelectedItem = Properties.Settings.Default.LastProtocol;
+            cmbLogType.SelectedItem = Properties.Settings.Default.LastLogType;
+            txtMessage.Text = Properties.Settings.Default.LastMessage;
+            numInterval.Value = Properties.Settings.Default.LastInterval;
+
+            this.FormClosing += Form1_FormClosing;
+
+            refreshTxtMessage();
+        }
+
+        private void refreshTxtMessage()
+        {
+            if (cmbLogType.SelectedItem.ToString() == "Custom")
+            {
+                txtMessage.Text = "";
+                txtMessage.Enabled = true;
+            }
+            else
+            {
+                txtMessage.Text = logSample[cmbLogType.SelectedItem.ToString()];
+                txtMessage.Enabled = false;
+            }
+        }
+
+        private void cmbLogType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            refreshTxtMessage();
+            Form1.ActiveForm.Text = $"{cmbLogType.SelectedItem.ToString()} - {appTitle}";
+        }
+
+        private void sendOnce_Click(object sender, EventArgs e)
+        {
+            string ip = txtIP.Text.Trim();
+            string portText = txtPort.Text.Trim();
+            string protocol = cmbProtocol.SelectedItem.ToString();
+
+            if (!IPAddress.TryParse(ip, out IPAddress ipAddress))
+            {
+                eventMsg.Text = "Invalid IP address.";
+                return;
+            }
+
+            if (!int.TryParse(portText, out int port) || port < 1 || port > 65535)
+            {
+                eventMsg.Text = "Invalid port number.";
+                return;
+            }
+
+            string selectedLine;
+
+            if (cmbLogType.SelectedItem.ToString() == "Fortigate")
+            {
+                selectedLine = GenerateFortigateLog();
+            }
+            else if (cmbLogType.SelectedItem.ToString() == "Mikrotik")
+            {
+                selectedLine = GenerateMikrotikLog();
+            }
+            else
+            {
+                messageLines = txtMessage.Lines.Where(line => !string.IsNullOrWhiteSpace(line)).ToList();
+                if (messageLines.Count == 0)
+                {
+                    eventMsg.Text = "Message must contain at least one non-empty line.";
+                    return;
+                }
+
+                Random rnd = new Random();
+                selectedLine = messageLines[rnd.Next(messageLines.Count)];
+            }
+
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(selectedLine);
+
+                if (protocol == "UDP")
+                {
+                    using (UdpClient udpClient = new UdpClient())
+                    {
+                        udpClient.Send(data, data.Length, ip, port);
+                    }
+                }
+                else if (protocol == "TCP")
+                {
+                    using (TcpClient tcpClient = new TcpClient())
+                    {
+                        tcpClient.Connect(ip, port);
+                        using (NetworkStream stream = tcpClient.GetStream())
+                        {
+                            stream.Write(data, 0, data.Length);
+                        }
+                    }
+                }
+
+                eventMsg.Text = "Log sent successfully!";
+            }
+            catch (Exception ex)
+            {
+                eventMsg.Text = $"Failed to send message: {ex.Message}";
+            }
+        }
+
+        private void btnSendAlways_Click(object sender, EventArgs e)
+        {
+            if (schedulerTimer.Enabled)
+            {
+                schedulerTimer.Stop();
+                btnSendAlways.Text = "Send Always";
+                SetInputState(true);
+                eventMsg.Text = "Stopped sending logs.";
+                return;
+            }
+
+            string ip = txtIP.Text.Trim();
+            string portText = txtPort.Text.Trim();
+            string protocol = cmbProtocol.SelectedItem.ToString();
+
+            if (!IPAddress.TryParse(ip, out IPAddress ipAddress))
+            {
+                eventMsg.Text = "Invalid IP address.";
+                return;
+            }
+
+            if (!int.TryParse(portText, out int port) || port < 1 || port > 65535)
+            {
+                eventMsg.Text = "Invalid port number.";
+                return;
+            }
+
+            if (cmbLogType.SelectedItem.ToString() == "Custom")
+            {
+                messageLines = txtMessage.Lines.Where(line => !string.IsNullOrWhiteSpace(line)).ToList();
+                if (messageLines.Count == 0)
+                {
+                    eventMsg.Text = "Message must contain at least one non-empty line.";
+                    return;
+                }
+            }
+
+            currentLineIndex = 0;
+            schedulerTimer.Tag = new SyslogContext { IP = ip, Port = port, Protocol = protocol };
+            schedulerTimer.Interval = (int)(numInterval.Value * 1000);
+            schedulerTimer.Start();
+
+            btnSendAlways.Text = "Stop";
+            SetInputState(false);
+            eventMsg.Text = "Started sending logs repeatedly.";
+        }
+
+        private void SchedulerTimer_Tick(object sender, EventArgs e)
+        {
+            if (cmbLogType.SelectedItem.ToString() == "Custom" && messageLines.Count == 0)
+            {
+                schedulerTimer.Stop();
+                btnSendAlways.Text = "Send Always";
+                SetInputState(true);
+                return;
+            }
+
+            var ctx = (SyslogContext)schedulerTimer.Tag;
+            string lineToSend;
+
+            if (cmbLogType.SelectedItem.ToString() == "Fortigate")
+            {
+                lineToSend = GenerateFortigateLog();
+            }
+            else
+            {
+                lineToSend = messageLines[currentLineIndex];
+                currentLineIndex++;
+                if (currentLineIndex >= messageLines.Count)
+                    currentLineIndex = 0;
+            }
+
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(lineToSend);
+
+                if (ctx.Protocol == "UDP")
+                {
+                    using (UdpClient udpClient = new UdpClient())
+                    {
+                        udpClient.Send(data, data.Length, ctx.IP, ctx.Port);
+                    }
+                }
+                else if (ctx.Protocol == "TCP")
+                {
+                    using (TcpClient tcpClient = new TcpClient())
+                    {
+                        tcpClient.Connect(ctx.IP, ctx.Port);
+                        using (NetworkStream stream = tcpClient.GetStream())
+                        {
+                            stream.Write(data, 0, data.Length);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                schedulerTimer.Stop();
+                btnSendAlways.Text = "Send Always";
+                SetInputState(true);
+                eventMsg.Text = $"Failed during scheduled send: {ex.Message}";
+            }
+        }
+
+        private void SetInputState(bool enabled)
+        {
+            txtIP.Enabled = enabled;
+            txtPort.Enabled = enabled;
+            cmbProtocol.Enabled = enabled;
+            cmbLogType.Enabled = enabled;
+            txtMessage.Enabled = enabled;
+            numInterval.Enabled = enabled;
+            sendOnce.Enabled = enabled;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.LastIP = txtIP.Text.Trim();
+            Properties.Settings.Default.LastPort = txtPort.Text.Trim();
+            Properties.Settings.Default.LastProtocol = cmbProtocol.SelectedItem?.ToString() ?? "UDP";
+            Properties.Settings.Default.LastLogType = cmbProtocol.SelectedItem?.ToString() ?? "Custom";
+            Properties.Settings.Default.LastMessage = txtMessage.Text;
+            Properties.Settings.Default.LastInterval = (int)numInterval.Value;
+            Properties.Settings.Default.Save();
+        }
+
+        private class SyslogContext
+        {
+            public string IP { get; set; }
+            public int Port { get; set; }
+            public string Protocol { get; set; }
+        }
+
+        public static string GetRandomNumber(int length)
+        {
+            long epochNow = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            return epochNow.ToString().Substring(epochNow.ToString().Length - length);
+        }
+
+        private string GenerateFortigateLog()
+        {
+            Random rand = new Random();
+
+            string datenow = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            string timenow = DateTime.UtcNow.ToString("HH:mm:ss");
+            long epochNow = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            string randomSip = ipCat1[rand.Next(ipCat1.Length)];
+            string randomDip = ipCat2[rand.Next(ipCat2.Length)];
+            string randomPort = ports[rand.Next(ports.Length)];
+
+            return $"date={datenow} time={timenow} logid=\"{GetRandomNumber(10)}\" type=\"traffic\" subtype=\"multicast\" level=\"notice\" vd=\"vdom{GetRandomNumber(1)}\" eventtime={epochNow} srcip={randomSip} srcport={randomPort} srcintf=\"port{GetRandomNumber(2)}\" srcintfrole=\"undefined\" dstip={randomDip} dstport={randomPort} dstintf=\"port{GetRandomNumber(2)}\" dstintfrole=\"undefined\" sessionid={GetRandomNumber(4)} proto={GetRandomNumber(2)} action=\"accept\" policyid=1 policytype=\"multicast-policy\" service=\"udp/{GetRandomNumber(4)}\" dstcountry=\"Reserved\" srccountry=\"Reserved\" trandisp=\"noop\" duration={GetRandomNumber(2)} sentbyte={GetRandomNumber(5)} rcvdbyte=0 sentpkt={GetRandomNumber(2)} rcvdpkt=0 appcat=\"unscanned\" ";
+        }
+
+        private string GenerateMikrotikLog()
+        {
+            Random rand = new Random();
+
+            string datenow = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            string shortMonth = DateTime.Now.ToString("MMM");
+            string shortDay = DateTime.Now.ToString("DD");
+            string timenow = DateTime.UtcNow.ToString("HH:mm:ss");
+            long epochNow = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            string randomSip = ipCat1[rand.Next(ipCat1.Length)];
+            string randomDip = ipCat2[rand.Next(ipCat2.Length)];
+            string randomOip = ipCat3[rand.Next(ipCat3.Length)];
+            string randomPort = ports[rand.Next(ports.Length)];
+
+            return $"{shortMonth} {shortDay} {timenow} {randomSip} id=\"firewall\" sn=C0EAE45CA55 time=\"{datenow} {timenow}\" fw={randomOip} pri={GetRandomNumber(1)} c={GetRandomNumber(1)} m=911 msg=\"Added host entry to dynamic address object\" n=6776584 n=6776584 note=\"FQDN=*.microsoft.com; TTL=56; Host={randomOip} fw_action=\"NA\"";
+        }
+    }
+}
